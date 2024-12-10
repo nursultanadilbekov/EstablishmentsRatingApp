f95af35a10ba7dc6487f1ee8976fdf5e88f14ee0
nursultan <nursultanadilbekov5@gmail.com>
2024-12-10 23:45:14 +0600

    first commit

--- Manga/src/main/java/com/example/manga/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.manga;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(MangaApp.class, args);
-    }
-}
